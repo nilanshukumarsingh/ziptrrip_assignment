ada1a36230e21ea748dcbf79b086732fad87291a
Nilanshu Kumar Singh <nilanshukumarsingh2005@gmail.com>
2026-06-25 11:53:14 +0530

    Add todos API endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const todoRoutes = require("./routes/todos");
 
 const app = express();
 const PORT = 3000;
+
 app.use(cors());
 app.use(express.json());
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend Working");
