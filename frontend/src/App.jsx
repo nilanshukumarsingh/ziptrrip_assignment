215454916faf64a4687fd5660769be148bae1a55
Nilanshu Kumar Singh <nilanshukumarsingh2005@gmail.com>
2026-06-25 12:35:41 +0530

    Add todo creation from frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "./api";
-import { useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const loadTodos = async () => {
+    const res = await api.get("/todos");
+    setTodos(res.data);
+  };
 
   useEffect(() => {
-    api.get("/todos").then((res) => setTodos(res.data));
+    loadTodos();
   }, []);
+
+  const addTodo = async () => {
+    if (!title) return;
+
+    await api.post("/todos", {
+      title: title,
+    });
+    setTitle("");
+    loadTodos();
+  };
 
   return (
     <div>
       <h1>Todo App</h1>
+
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
       {todos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
       ))}
