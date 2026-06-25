18095494e2aaaa1aeb3f600f6caa1f20810908d1
Nilanshu Kumar Singh <nilanshukumarsingh2005@gmail.com>
2026-06-25 11:48:09 +0530

    Add JSON storage for todos

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend Working");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
