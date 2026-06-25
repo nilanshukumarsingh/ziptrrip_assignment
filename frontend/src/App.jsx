fc8b5b8d863419fcf89b8296510a4c2e98df2b34
Nilanshu Kumar Singh <nilanshukumarsingh2005@gmail.com>
2026-06-25 11:46:39 +0530

    Connect React with Express API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import api from "./api";
+
 function App() {
+  useEffect(() => {
+    api.get("/").then((res) => console.log(res.data));
+  }, []);
+
   return <h1>Todo App</h1>;
 }
 
